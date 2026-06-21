4a54ac3478a4814df997882e79c1fadc27b728c1
Quizalto CI <ci@quizalto.com>
2026-06-21 12:20:47 +0000

    Update formula to fb007b2

--- Formula/quizalto.rb
+++ Formula/quizalto.rb
@@ -1,17 +1,17 @@
 class Quizalto < Formula
   desc "Quizalto CLI - Interact with the Quizalto API from your terminal"
   homepage "https://www.quizalto.com"
-  version "024141d"
+  version "fb007b2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://www.quizalto.com/assets/v024141d/quizalto-darwin-arm64"
-      sha256 "8338a5c9e01cb6c5fdcd5c4d22e2798e225b09fa0f7956ff4d09d2b1764a08bd"
+      url "https://www.quizalto.com/assets/vfb007b2/quizalto-darwin-arm64"
+      sha256 "c51f79ce40788f53e8708f9dbea314a12e8d78b05dcc78b5c2a161af2f9cf888"
     end
     on_intel do
-      url "https://www.quizalto.com/assets/v024141d/quizalto-darwin-amd64"
-      sha256 "82aeebbb76dd88538d8562d9a7204a141f31ee0e9526397a9796c65dd1c3cf05"
+      url "https://www.quizalto.com/assets/vfb007b2/quizalto-darwin-amd64"
+      sha256 "5d09de0d8f7e75eaac45c059344ec05fbae1a78c9ede937e30d49cf9844c573a"
     end
   end
 
---
+++ README.md
@@ -0,0 +1,9 @@
+# Homebrew Tap for Quizalto CLI
+
+## Install
+
+```bash
+brew tap f-quiz/quizalto https://github.com/f-quiz/homebrew-quizalto.git --branch PR-271 && brew install quizalto
+```
+
+Branch: PR-271
