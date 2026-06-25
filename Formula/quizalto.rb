5c9a5cd2c1c35ed92fbe2c46d3729ee4c1a5fc7b
Quizalto CI <ci@quizalto.com>
2026-06-25 10:39:55 +0000

    Update formula to abe27f1

--- Formula/quizalto.rb
+++ Formula/quizalto.rb
@@ -1,17 +1,17 @@
 class Quizalto < Formula
   desc "Quizalto CLI - Interact with the Quizalto API from your terminal"
   homepage "https://www.quizalto.com"
-  version "8b0e303"
+  version "abe27f1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://www.dev.quizalto.com/assets/v8b0e303/quizalto-darwin-arm64"
-      sha256 "1addd3b25b7cbbb0887dac7f4c9971971e3b8f1459d043d68b0e0eff6aa9442c"
+      url "https://www.dev.quizalto.com/assets/vabe27f1/quizalto-darwin-arm64"
+      sha256 "654188667f23fb55e62db90abc6299dd0d9aa04aced68db3bbb75ce45bfbe16f"
     end
     on_intel do
-      url "https://www.dev.quizalto.com/assets/v8b0e303/quizalto-darwin-amd64"
-      sha256 "952c937dacd4931867f560d677405ba22092099566cb63b35307427ca91cd319"
+      url "https://www.dev.quizalto.com/assets/vabe27f1/quizalto-darwin-amd64"
+      sha256 "016a9b08f5c1c9aec6b14cd04250af1bc010321f453d22ea052eec76bfc0d7bc"
     end
   end
 
