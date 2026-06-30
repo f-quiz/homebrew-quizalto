0fb14909075f9e9eb3e21718286215016ba35bee
Quizalto CI <ci@quizalto.com>
2026-06-30 10:19:05 +0000

    Update formula to 11c3e31

--- Formula/quizalto.rb
+++ Formula/quizalto.rb
@@ -1,17 +1,17 @@
 class Quizalto < Formula
   desc "Quizalto CLI - Interact with the Quizalto API from your terminal"
   homepage "https://www.quizalto.com"
-  version "a3ed846"
+  version "11c3e31"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://www.quizalto.com/assets/va3ed846/quizalto-darwin-arm64"
-      sha256 "1cd30d8259cfc7218039e4d2a3e73419b38712ea87df145848473c2fc608ec8a"
+      url "https://www.dev.quizalto.com/assets/v11c3e31/quizalto-darwin-arm64"
+      sha256 "763e5a7fa409e5ece4ff08e6abd96007a906775e5da209c7d1e147831c07691f"
     end
     on_intel do
-      url "https://www.quizalto.com/assets/va3ed846/quizalto-darwin-amd64"
-      sha256 "1319f2c1dfc67c57042e9b7c482f445afc37921e835b3b84172b3cb5d8d3de1d"
+      url "https://www.dev.quizalto.com/assets/v11c3e31/quizalto-darwin-amd64"
+      sha256 "4f4007c39753268fb4dcc690795a4540e30cd7ae4e1fb258f9870a269aa6e71d"
     end
   end
 
--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 ## Install
 
 ```bash
-brew tap f-quiz/quizalto && brew install quizalto
+brew tap f-quiz/quizalto
+cd "$(brew --repo f-quiz/quizalto)" && git fetch origin PR-279 && git checkout PR-279
+brew install quizalto
 ```
 
-Branch: main
+Branch: PR-279
