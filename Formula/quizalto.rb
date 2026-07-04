4e271139b36b64bc59c83d0e4d972b8851e6f660
Quizalto CI <ci@quizalto.com>
2026-07-04 10:42:57 +0000

    Update formula to 5c32cbf

--- Formula/quizalto.rb
+++ Formula/quizalto.rb
@@ -1,17 +1,17 @@
 class Quizalto < Formula
   desc "Quizalto CLI - Interact with the Quizalto API from your terminal"
   homepage "https://www.quizalto.com"
-  version "0fe247a"
+  version "5c32cbf"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://www.quizalto.com/assets/v0fe247a/quizalto-darwin-arm64"
-      sha256 "e3a41c4e0d97460c6264f9fa7ae4b7973e1ab5c6301c39f9949238c3cd3eb565"
+      url "https://www.dev.quizalto.com/assets/v5c32cbf/quizalto-darwin-arm64"
+      sha256 "dc577048804d62f48a4d317ebe525e393b7c81d20dfc02725cacc85d9ea6734c"
     end
     on_intel do
-      url "https://www.quizalto.com/assets/v0fe247a/quizalto-darwin-amd64"
-      sha256 "e58bc6ae7c0a6cda8033d4ca6531bfdb8b9db20d7c731c769dc787f4aa3c3f98"
+      url "https://www.dev.quizalto.com/assets/v5c32cbf/quizalto-darwin-amd64"
+      sha256 "04137c28a3fcabd1da3f24b233608f4f66ee39054663d0a31f442cbe0c9a088b"
     end
   end
 
