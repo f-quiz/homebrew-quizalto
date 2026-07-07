61bc4364a4eced36080186878a84ddbff8042868
Quizalto CI <ci@quizalto.com>
2026-07-07 20:13:40 +0000

    Update formula to 59ba453

--- Formula/quizalto.rb
+++ Formula/quizalto.rb
@@ -1,17 +1,17 @@
 class Quizalto < Formula
   desc "Quizalto CLI - Interact with the Quizalto API from your terminal"
   homepage "https://www.quizalto.com"
-  version "676bf5a"
+  version "59ba453"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://www.dev.quizalto.com/assets/v676bf5a/quizalto-darwin-arm64"
-      sha256 "c11f9b9b7919a23ea33946051757732d752ce69cc4795df4fa65ae2b9ae0742a"
+      url "https://www.dev.quizalto.com/assets/v59ba453/quizalto-darwin-arm64"
+      sha256 "4318797ae76efa10f1741a9c721e349a0f1418ad8bc658f6dbd4409781a9e1a4"
     end
     on_intel do
-      url "https://www.dev.quizalto.com/assets/v676bf5a/quizalto-darwin-amd64"
-      sha256 "767ff9cc91529a508affcec6c0eaaaae5b6eb41240b4fe3c20e40ba917a352f7"
+      url "https://www.dev.quizalto.com/assets/v59ba453/quizalto-darwin-amd64"
+      sha256 "e4c6c78c08e52934054ab1f44d162772a9534f9e530646976b677269bb99ec52"
     end
   end
 
