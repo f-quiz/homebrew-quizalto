87a9b02b4cced3c55c215a901e68213e9cd2ecbd
Quizalto CI <ci@quizalto.com>
2026-08-08 07:06:15 +0000

    Update formula to a090327

--- Formula/quizalto.rb
+++ Formula/quizalto.rb
@@ -7,11 +7,11 @@ class Quizalto < Formula
   on_macos do
     on_arm do
       url "https://www.dev.quizalto.com/assets/va090327/quizalto-darwin-arm64"
-      sha256 "92cf9665a0836690b913fdb735e813e5a464bbf59d2c79a7e35ce5e2b4006b4d"
+      sha256 "58c81019568990023873be0833b0f049eed7b42a25186e30256ab13523d9993e"
     end
     on_intel do
       url "https://www.dev.quizalto.com/assets/va090327/quizalto-darwin-amd64"
-      sha256 "eb33b5e05636572d734166a309f2104cfe720790bde2798b6759696287b08b79"
+      sha256 "758cb589754e6094e0131ee3bc80375641a0adf72a2fc9378a94d06ad3e93aec"
     end
   end
 
