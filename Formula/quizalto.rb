180f63006977a026a4754590d33fcc31a4133f39
Quizalto CI <ci@quizalto.com>
2026-08-11 06:00:45 +0000

    Update formula to 3b68867

--- Formula/quizalto.rb
+++ Formula/quizalto.rb
@@ -7,11 +7,11 @@ class Quizalto < Formula
   on_macos do
     on_arm do
       url "https://www.dev.quizalto.com/assets/v3b68867/quizalto-darwin-arm64"
-      sha256 "1498c08ff1aef4a2df7d376a64674b87b1da7a6a4c0c4ee822536b74544b955e"
+      sha256 "33470333d8c8a0a8b0a0aa094bb1aa57270e30c1a723b5a118b71d8f2aa892ff"
     end
     on_intel do
       url "https://www.dev.quizalto.com/assets/v3b68867/quizalto-darwin-amd64"
-      sha256 "c3226f3362c5fe733fc5e7de030527761e8600fba93c2641972eb6238acaf113"
+      sha256 "051eedc2dbf04683d3824468ecfb1d50883d6d761ac37d0c46109efde635bcef"
     end
   end
 
