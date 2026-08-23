9181c60307819dd0df1610978e01a06445803ff0
Quizalto CI <ci@quizalto.com>
2026-08-23 17:21:15 +0000

    Update formula to 4a2a2fc

--- Formula/quizalto.rb
+++ Formula/quizalto.rb
@@ -7,11 +7,11 @@ class Quizalto < Formula
   on_macos do
     on_arm do
       url "https://www.dev.quizalto.com/assets/v4a2a2fc/quizalto-darwin-arm64"
-      sha256 "166f362b1310edac7e19e626add5652c1c52a2e04657d51da72bdb67c884b3be"
+      sha256 "2c4452fe25351a7994db294601600ac7f1568e6988724d0528f869b333f1f87d"
     end
     on_intel do
       url "https://www.dev.quizalto.com/assets/v4a2a2fc/quizalto-darwin-amd64"
-      sha256 "a308f5553eaaaeb7f886e222437f07034788b5711fafbcd6aad75cd321e24f85"
+      sha256 "62e8ff5263254e71fb29682e77d2bbeb7a50009042b8bcbcf60ffeed96f2ba31"
     end
   end
 
